01b5bda0f4dda3768099eddfc3db0461fc7b34bc
natashafjy <natashafjy@gmail.com>
2023-04-08 18:08:39 +0800

    added get_island_rain_probability to backend file, updated return values for /results

--- backend/flask-app/backend.py
+++ backend/flask-app/backend.py
@@ -2,7 +2,9 @@ from flask import Flask, jsonify, request
 import mysql.connector
 import xgboost as xgb
 from get_rain_probability import *
+from get_island_rain_probability import *
 from get_last_rain import *
+
 
 app = Flask(__name__)
 
@@ -166,22 +168,22 @@ def make_prediction():
     '''
     To be implemented
     '''
-    # 1. get stations near points in routine
 
-    # 2. retrieve data from API and format data to fit into model
-    # current_date= date.today()
-    # input_time_start = ###
-    # input_time_end = ###
+    # 1. retrieve data from API and format data to fit into model
+
+
+
+
     
  
-    # 3. use model to generate predictions
+    # 2. use model to generate predictions
     xgboost_model = xgb.XGBRegressor()
     xgboost_model.load_model("xgboost_model.json")
     predicted_values = xgboost_model.predict(formatted_data) # replace with variable name of formatted data from (2)
     stations = [] # replace with list of stations
     predicted_data = pd.DataFrame(list(zip(stations, predicted_values)), columns=["stations", "predicted_values"])
 
-    # 4. generate probabilities
+    # 3. generate probabilities
     # Assumes GET request has data = {"user_name": username, "routine_id": id}
     username, routine_id = request.form["user_name"], request.form["routine_id"]
     
@@ -200,9 +202,11 @@ def make_prediction():
     cursor.close()
     db.close()
 
-    result = get_rain_probability(predicted_data, points_of_interest)
+    result1 = get_rain_probability(predicted_data, points_of_interest)
+    result2 = get_island_rain_probability(predicted_data)
 
+    # 4. find most recent instance of rain at the start point and end point of user's routine
     last_rain_start, last_rain_end = get_last_rain(points_of_interest)
     
-    return result
+    return result1, result2, last_rain_start, last_rain_end
 
