fec31370982374dbf88d9aeeda506b6317bf1758
natashafjy <64476154+natashafjy@users.noreply.github.com>
2023-04-11 15:48:05 +0800

    Update get_routine_rain_probability.py

--- frontend/get_routine_rain_probability.py
+++ frontend/get_routine_rain_probability.py
@@ -53,30 +53,21 @@ def get_routine_rain_probability(predicted_data, points_of_interest):
             # calculate standard deviation, and calculate probability using CDF
             rain_probability_df["sd"] = np.sqrt(rain_probability_df["variance"])
             rain_probability_df["P=0"] = scipy.stats.norm(loc=rain_probability_df["rain"], scale=rain_probability_df["sd"]).cdf(0)
-            rain_probability_df["predicted_rain"] = round(rain_probability_df["rain"], 1)
+            # rain_probability_df["predicted_rain"] = round(rain_probability_df["rain"], 1)
             # no need: rain_probability_df["P=rain_rounded"] = scipy.stats.norm(loc=rain_probability_df["rain"], scale=rain_probability_df["sd"]).cdf(rain_probability_df["rain_rounded"])
 
             # add precipitation values and their associated probabilities to df
             prob_of_rain = round(1 - rain_probability_df["P=0"].mean(), 3)
 
             if point_num == 0:
-                start_point_pred_lst.append([point_long, point_lat, time, rain_probability_df["predicted_rain"].mean(), prob_of_rain])
+                start_point_pred_lst.append([point_long, point_lat, time, round(rain_probability_df["rain"].mean(), 1), prob_of_rain])
             else: 
-                end_point_pred_lst.append([point_long, point_lat, time, rain_probability_df["predicted_rain"].mean(), prob_of_rain])
+                end_point_pred_lst.append([point_long, point_lat, time, round(rain_probability_df["rain"].mean(), 1), prob_of_rain])
 
     # convert list of lists into dataframe
     start_point_pred_df = pd.DataFrame(start_point_pred_lst, columns=["longitude", "latitude", "time", "predicted_rain", "P(predicted_rain > 0)"])
     end_point_pred_df = pd.DataFrame(end_point_pred_lst, columns=["longitude", "latitude", "time", "predicted_rain", "P(predicted_rain > 0)"])
-
     return start_point_pred_df, end_point_pred_df
 
-def print_example():
-    sample_data = pd.DataFrame({"time": [0, 0, 0, 0, 0, 0, 5, 5, 5, 5, 5, 5, 10, 10, 10, 10, 10, 10, 15, 15, 15, 15, 15, 15, 20, 20, 20, 20, 20, 20, 25, 25, 25, 25, 25, 25],
-                                "station": ["S71", "S92", "S203", "S230", "S223", "S226", "S71", "S92", "S203", "S230", "S223", "S226", "S71", "S92", "S203", "S230", "S223", "S226", "S71", "S92", "S203", "S230", "S223", "S226", "S71", "S92", "S203", "S230", "S223", "S226", "S71", "S92", "S203", "S230", "S223", "S226"],
-                                "value": [0.5, 0.7, 0.8, 1.4, 0.2, 0.2, 0.4, 0.3, 0.6, 1.0, 0.0, 0.0, 0.1, 0.3, 0.6, 0.7, 0.3, 0.4, 0.5, 0.7, 0.8, 1.4, 0.2, 0.2, 0.8, 0.9, 1.2, 1.0, 1.2, 1.4, 0.7, 0.9, 1.2, 0.8, 0.6, 0.6]})
-    print("This is a tuple of dataframes:")
-    print(get_rain_probability(sample_data, [(104.78150, 1.29230), (103.80389, 1.26472)]))
-
-# print_example()
 
     
