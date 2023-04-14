1070cc005318242703e17be07936fa0382bfa5af
natashafjy <natashafjy@gmail.com>
2023-04-14 15:04:03 +0800

    added unit tests for MLP metrics calculation

---
+++ backend/tests/test_MLP_model.py
@@ -0,0 +1,34 @@
+import math
+from sklearn.metrics import confusion_matrix
+
+import sys
+sys.path.insert(0, "backend/models/save_models")
+from MLPRegressor import evaluate
+
+def test_MLP_model():
+
+    y_pred = [1.0, 1.0, 1.0, 0, 0, 1.0, 1.0, 0, 0]
+    y_true = [1.0, 1.0, 0, 0, 1.0, 0, 1.0, 1.0, 1.0]
+
+    cm = confusion_matrix(y_true, y_pred)
+
+    test_TN = cm[0][0]
+    test_FN = cm[1][0]
+    test_TP = cm[1][1]
+    test_FP = cm[0][1]
+
+    test_fnr = (test_FN) / (test_FN + test_TP)
+    test_fpr = (test_FP) / (test_FP + test_TN)
+    test_f1 = (test_TP) / (test_TP + 0.5 * (test_FP + test_FN))
+    fnr, fpr, f1 = evaluate(y_pred, y_true)
+
+    assert(math.isclose(test_fnr, fnr))
+    assert(math.isclose(test_fpr, fpr))
+    assert(math.isclose(test_f1, f1))
+
+    assert ((fnr >= 0) and (fnr <= 1))
+    assert ((fnr >= 0) and (fnr <= 1))
+    assert ((fnr >= 0) and (fnr <= 1))
+
+
+    
